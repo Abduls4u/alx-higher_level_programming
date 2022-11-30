6af8665397732fb11f362e936bf359859468a352
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2022-11-30 03:00:52 -0800

    lower case letters

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,6 +9,6 @@ else:
 if last > 5:
     print(f"Last digit of {number} is {last} and is greater than 5")
 elif last == 0:
-    print(f"Last digit of {number} is {last} and is zero")
-elif (last < 6 and not(last == 0)):
-    print(f"Laat digit of {number} is {last} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {last} and is 0")
+elif (last < 6 and last != 0):
+    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
