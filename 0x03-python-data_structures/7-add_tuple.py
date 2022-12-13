a175c1be5503bccbb9b4809818b8b98e287c4b4f
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2022-12-13 09:39:10 -0800

    fixed errors

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) == 1 or len(tuple_b) == 1:
+        tuple_a[1] = 0
+        tuple_b[1] = 0
+    elif len(tuple_a) < 1 or len(tuple_b) < 1:
+        tuple_a[0] = 0
+        tuple_b[0] = 0
+        tuple_a[1] = 0
+        tuple_b[1] = 0
+
     add_1 = tuple_a[0] + tuple_b[0]
     add_2 = tuple_a[1] + tuple_b[1]
     total = (add_1, add_2)
