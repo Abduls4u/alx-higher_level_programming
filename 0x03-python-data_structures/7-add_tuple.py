6e3f5f76d072a3ee0dc6a241da5fcc3653f89f81
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2022-12-13 09:15:29 -0800

    sum of tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,5 +2,5 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     add_1 = tuple_a[0] + tuple_b[0]
     add_2 = tuple_a[1] + tuple_b[1]
-    total = ('add_1', 'add_2')
+    total = (add_1, add_2)
     return (total)
