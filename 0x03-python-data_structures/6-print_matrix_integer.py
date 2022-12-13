97f523f09b8848d7efd812abb6cd113c754970d7
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2022-12-13 03:38:53 -0800

    used nested loops

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    matrix = [[k for k in range(3)] for i in range(3)]
-    print("{:d}".format(matrix))
+    for r in matrix:
+        for i in r:
+	        print("{:d}".format(i), end=" ")
+    print()
