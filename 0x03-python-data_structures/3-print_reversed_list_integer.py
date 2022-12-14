389f06950aea2f1d0cf727cb754d8952fef36bf2
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2022-12-14 05:58:16 -0800

    What do u think about reverse engineering

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,5 +3,5 @@ def print_reversed_list_integer(my_list=[]):
     if my_list is None:
         return ()
     else:
-        for i in my_list.reverse():
+        for i in reversed(my_list):
             print("{:d}".format(i))
