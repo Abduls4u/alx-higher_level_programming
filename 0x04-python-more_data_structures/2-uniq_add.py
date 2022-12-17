6a2a3e4fbd845def63d9e68413722028b7b9f0a1
Abduls4u~ <abdulsalamabdulsomad14@gmail.com>
2022-12-17 02:49:58 -0800

    we all strive to be unique

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    for i in range(0, len(my_list)):
+    for i in range(len(my_list) - 1):
         if my_list[i] != my_list[i+1]:
             my_list[i] += my_list[i+1]
     return (a)
