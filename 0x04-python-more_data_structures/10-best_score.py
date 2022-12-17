e01cbc5867cf75cd7d008853673f59a6fb4615ab
Abduls4u~ <abdulsalamabdulsomad14@gmail.com>
2022-12-17 10:28:55 -0800

    Biggest key

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return ()
+    else:
+        Best = {'Name': None, 'Score': 0}
+        for d, i in a_dictionary.items():
+            if i > Best['Score']:
+                Best['Name'] = d
+                Best['Score'] = i
+        return (Best['Name'])
