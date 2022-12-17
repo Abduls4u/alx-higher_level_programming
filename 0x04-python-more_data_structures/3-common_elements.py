fe7fedc770b82fbb0fe6943bce40279b505b1f06
Abduls4u~ <abdulsalamabdulsomad14@gmail.com>
2022-12-17 03:37:57 -0800

    common enemy

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    set_3 = set()
-    for i in set_1:
-        for i in set_2:
-            if i in set_1 and i in set_2:
-                set_3 = set_3.add(i)
-    return (set_3)
+    return (set_1 and set_2)
