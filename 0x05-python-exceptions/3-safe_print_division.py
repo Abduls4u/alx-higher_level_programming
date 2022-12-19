b3ecdb776542a0a59d6a29bef94a46c8c32d9796
Abduls4u~ <abdulsalamabdulsomad14@gmail.com>
2022-12-19 06:28:14 -0800

    safe print division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    if b == 0:
-        c = None
-    else:
-        c = a / b
+    c = None
     try:
-        print("Inside result: {}".format(c))
+        c = a / b
     except:
         pass
     finally:
-        print("{} / {} = {}".format(a, b, c))
+        print("Inside result: {}".format(c))
