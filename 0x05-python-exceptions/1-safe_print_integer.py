529f27ec1672ab327280fd5e82738ae46358e44a
Abduls4u~ <abdulsalamabdulsomad14@gmail.com>
2022-12-19 08:24:15 -0800

    Objection my checker I believe there should be an EXCEPTION while checking my codes

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,8 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        return (True)
     except ValueError:
-        pass
         return(False)
+    else:
+        return (True)
     print()
