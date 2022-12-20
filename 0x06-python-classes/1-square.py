193f589c4a9f3dc5d9791ef7d20b93c0a7d4bf7d
Abduls4u~ <abdulsalamabdulsomad14@gmail.com>
2022-12-20 15:34:54 -0800

    unsettled

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+''' A class that defines a square.
+Usage:
+    ./0-square.py
+Author:
+    Abdulsalam Abdulsomad .A. - December 20th, 2022.
+'''
+
+
+class Square:
+    ''' A class that represents a square.'''
+
+    def __init__(self, size=0):
+        '''Contains all the necessary attributes for the square object.
+        parameters:
+        ___________
+             size(int): size of a square.
+        '''
+
+    self.__size = size
