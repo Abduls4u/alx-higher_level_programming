a57fbb919aa29da36417ca6e7402f7c1bdb832bf
Abduls4u~ <abdulsalamabdulsomad14@gmail.com>
2022-12-20 14:52:19 -0800

    first docstring umentation/ empty square class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' A class that defines a square.
+
+Usage:
+    ./0-square.py
+
+Author:
+    Abdulsalam Abdulsomad .A. - December 20th, 2022.
+'''
+class Square:
+'''' An empty class that represents a square.'''
+
+    pass
