e25eac64838a6d1764e2f3cf6f47493569dcf6e4
Abduls4u~ <abdulsalamabdulsomad14@gmail.com>
2022-12-21 18:09:08 -0800

    class square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -10,11 +10,12 @@ Author:
 class Square:
     ''' A class that represents a square.'''
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         '''Constructs all the necessary attributes for the square object.
         parameters:
         ___________
             size(int): size of a square.
+            position(tuple): coordinates of a square.
         '''
 
         self.__size = size
@@ -22,6 +23,40 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
+
+        self.__position = position
+        if not type(position) is tuple or len(position) < 2:
+            if position[0] < 0 or position[1] < 0:
+                raise TypeError("position must be a tuple of \
+2 positive integers")
+
+    @property
+    def position(self):
+        '''getter method to retrieve position.'''
+
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        '''setter method to set value.'''
+        if not type(position) is tuple or len(position) < 2:
+            if position[0] < 0 or position[1] < 0:
+                raise TypeError("position must be a tuple of \
+2 positive integers")
+
+    @property
+    def size(self):
+        '''Getter method to retrieve '''
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        '''setter method to set value.'''
+        if not type(value) is int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         ''' returns the area of a square.
@@ -31,3 +66,13 @@ class Square:
             square of size.
         '''
         return (self.__size ** 2)
+
+    def my_print(self):
+        '''Prints a square to stdout'''
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print('#', end="")
+                print()
