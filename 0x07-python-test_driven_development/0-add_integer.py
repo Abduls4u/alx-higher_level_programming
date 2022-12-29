f042f3e2b989c31f8e8ef8c0907aeebaf7316718
Abduls4u~ <abdulsalamabdulsomad14@gmail.com>
2022-12-29 04:12:55 -0800

    created README file, tests directory and more importantly a module that contains a function that adds two ints

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+''' A module that contains a function that adds 2 integers.
+Usage:
+    ./0-add_integer
+Author:
+    Abdulsalam Abdulsomad .A. - December 29th, 2022.
+'''
+
+
+def add_integer(a, b=98):
+    ''' A function that returns the sum of two integers.
+    parameters:
+    ***********
+        a(int): first argument.
+        b(int): second argument.
+    Return:
+    *******
+        type == int.
+           The sum of the two integers passed as arguments.
+    Raise:
+    ******
+        TypeError: if arguments are not int.
+    '''
+
+    if (not isinstance(a, int)) and (not isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    if (not isinstance(b, int)) and (not isinstance(b, float)):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return (a + b)
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+This directory contains all my 0x07-python-test_driven_development project's solutions
