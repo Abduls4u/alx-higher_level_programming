99ff263780cd4c08f0c3d2f6226a3b3f4e774ab6
Abduls4u~ <abdulsalamabdulsomad14@gmail.com>
2023-01-02 13:40:44 -0800

    class Rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+''' A class that defines a rectangle.
+Usage:
+    ./0-rectangle.py
+Author:
+    Abdulsalam Abdulsomad .A. - January 2nd, 2023.
+'''
+
+
+class Rectangle:
+    ''' An empty class that represents a rectangle.'''
+
+
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+This directory contains all my 0x08-python-more_classes project solutions.
