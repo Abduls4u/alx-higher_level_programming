de2021121eec839ea3723668e027a74e7a0557fd
Abduls4u~ <abdulsalamabdulsomad14@gmail.com>
2023-01-02 15:02:36 -0800

    class Rectangle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -65,4 +65,7 @@ class Rectangle:
         Return:
             2(width + height).
         '''
-        return (2 * (self.__width + self.__height))
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        else:
+            return (2 * (self.__width + self.__height))
