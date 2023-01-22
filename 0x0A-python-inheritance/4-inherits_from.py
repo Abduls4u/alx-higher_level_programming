ac6110922a287c715e5d5cd4ec43fe99dddd211d
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2023-01-22 02:35:53 -0800

    Wrote a function that returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class ; otherwise False

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,23 @@
+''' A module that contains a function that tests if an object is
+a subclass.
+Usage:
+    ./4-inherits_from.py
+Author:
+    Abdulsalam Abdulsomad .A. - January 22nd, 2023.
+'''
+
+
+def inherits_from(obj, a_class):
+    ''' returns True if the object is an an instance of a class
+that inherited (directly or indirectly) from the specified class
+;otherwise False
+    parameters:
+    -----------
+        obj: object to test.
+        a_class: class to be specified.
+    '''
+
+    if issubclass(type(obj), a_class) and not type(obj) is a_class:
+        return (True)
+    else:
+        return (False)
