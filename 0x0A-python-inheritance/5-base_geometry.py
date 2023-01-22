3a333c79cc98c4ed1994bfebb0d0e71f08dbb5d6
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2023-01-22 09:17:51 -0800

    Created an empty class

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' A module that contains an empty class.
+Usage:
+    ./5-base_geometry.py
+Author:
+    Abdulsalam Abdulsomad .A. - January 22nd, 2023.
+'''
+
+
+class BaseGeometry:
+    ''' An empty class.'''
+
+    pass
