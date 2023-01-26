fbf35c0d6939a12d42f5d402df951caa4e46b586
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2023-01-26 02:50:18 -0800

    Wrote and tested a function that reads a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+''' Contains a function that reads a file.
+Usage:
+    ./0-read_file.py
+Author:
+    Abdulsalam Abdulsomad .A. - January 26th, 2023.
+'''
+
+
+def read_file(filename=""):
+    ''' A function that reads a text file (UTF8) and prints it to stdout
+    Parameters:
+        -----------
+        filename(str) = Name of the file to read and print.
+    '''
+
+    with open(filename, 'r', encoding='utf-8') as myfile:
+        print(myfile.read())
