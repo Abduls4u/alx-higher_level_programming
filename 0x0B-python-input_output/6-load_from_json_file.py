e864ec097a25e06556ee09dcb0cbf5d31e76cbe2
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2023-01-27 09:14:07 -0800

    wrote a fumcyiom thay creayes an object from a json file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+''' Contains a function that creates an object from a json file.
+Usage:
+    ./5-load_from_json_file.py
+Author:
+    Abdulsalam Abdulsomad .A. - January 27th, 2023.
+'''
+import json
+
+
+def load_from_json_file(filename):
+    ''' A function that creates an Object from a “JSON file”
+    Parameters:
+    -----------
+        filename(str): Name of the file to write to.
+    '''
+
+    with open(filename):
+        return (json.load(filename))
