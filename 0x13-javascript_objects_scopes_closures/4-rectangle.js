e866503f036cb6ecb3e0082bb9130640b1dabeac
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2023-03-19 04:59:59 -0700

    Wrote scripts that creates a class(Square) that inherits from class(Rectangle) and a method that prints a square with the char passed as argument

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((!(w <= 0) && (w !== undefined)) && (!(h <= 0) && (h !== undefined))) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      console.log();
+    }
+  }
+
+  rotate () {
+    let swap = this.height;
+    this.height = this.width;
+    this.width = swap;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+module.exports = Rectangle;
