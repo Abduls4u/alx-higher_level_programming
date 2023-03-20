e9673a8bd2fad25c5940df0a65491d0369cec510
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2023-03-20 06:35:20 -0700

    wrote scripts that displays the contents of a table(states) in a db.

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,13 +7,14 @@ Author:
     Abdulsalam Abdulsomad .A. - March 20th, 2023.
 '''
 import MySQLdb as sql
+import sys
 
 
 if __name__ == "__main__":
-    db = sql.connect(host="localhost", port=3306, user="root",
-                     passwd="root", db="hbtn_0e_0_usa")
+    db = sql.connect( user=sys.argv[1],
+                     passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT id, name FROM states ORDER BY states.id ASC")
+    cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
     results = cursor.fetchall()
     for row in results:
         print(row)
