a980c2fe0888bf78c272442f656283d38955785c
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2023-03-20 13:38:25 -0700

    Debugged and made it fit the project requirements

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,7 +11,7 @@ import sys
 
 
 if __name__ == "__main__":
-    db = sql.connect( user=sys.argv[1],
+    db = sql.connect(user=sys.argv[1],
                      passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,11 +7,12 @@ Author:
     Abdulsalam Abdulsomad .A. - March 20th, 2023.
 '''
 import MySQLdb as sql
+import sys
 
 
 if __name__ == "__main__":
-    db = sql.connect(host="localhost", port=3306, user="root",
-                     passwd="root", db="hbtn_0e_0_usa")
+    db = sql.connect(host="localhost", port=3306, user=sys.argv[1],
+                     passwd=sys.argv[1], db=sys.argv[3])
     cursor = db.cursor()
     query = "SELECT id, name FROM states WHERE name LIKE %s ORDER BY id ASC"
     cursor.execute(query, ('N%',))
