ce2e2cb1fe2a0162e5aadb2bab6e7da123446dba
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2023-03-24 04:36:39 -0700

    Wrote a script that lists all the states object

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,25 +1,25 @@
- #!/usr/bin/python3
-''' A module that contains  script that lists all cities in a
-state from the database.
+#!/usr/bin/python3
+''' A module that contains  script that lists all State objects.
 Usage:
-    ./model_states.py
+    ./7-model_state_fetch_all.py
 Author:
-    Abdulsalam Abdulsomad .A. - March 20th, 2023.
+    Abdulsalam Abdulsomad .A. - March 24th, 2023.
 '''
 
+
 import sys
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 from model_state import Base, State
+
 from sqlalchemy import (create_engine)
 
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                            .format(sys.argv[1], sys.argv[2],
-                             sys.argv[3]), pool_pre_ping=True)
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    states = session.query(State).order_by(State.id).all()
+    session = Session(bind=engine)
+    states = session.query(State.id, State.name).order_by(State.id)
     for state in states:
-        print(f'{State.id}: {State.name}')
+        print(f'{state.id}: {state.name}')
