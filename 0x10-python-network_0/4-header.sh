0c1155aa38a9ad635456249bad202de5cf6a5cf7
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2023-04-07 10:15:41 -0700

    wrote a script that creates a headet

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to a URL
-curl -I -X HEAD -s -D - "$1" | awk '/^Allow:/ {print substr($0, 8)}' 
+curl -sI -H "X-School-User-Id: 98" "$1"
