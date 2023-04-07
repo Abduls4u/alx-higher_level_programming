a678845ab7bf737f63d07f67c7ef50a7b238b2b5
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2023-04-07 10:41:08 -0700

    wrote a script that sends a POST request and data to the url passed

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to a URL
-curl -d "email=test@gmail.com&subject=I will always be here for PLD"-sL -X POST "$1"
+curl -d "email=test@gmail.com&subject=I will always be here for PLD" -X POST -s "$1"
