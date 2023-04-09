d427127c2a657a60b0a7ecbaa1e149b17da5e0e0
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2023-04-09 02:21:47 -0700

    wrote a script that sends a POST request to a url using the mail passed as a parameter

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-'''A Python script that takes in a URL and an email, sends a POST 
-request to the passed URL with the email as a parameter,
-and displaysthe body of the response (decoded in utf-8)
+'''Sends a POST request to a url.
 Usage:
    ./2-post_email.py <url> <mail>
 Author:
@@ -12,13 +10,12 @@ import urllib.parse as parse
 import sys
 
 
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        url = sys.argv[1]
-        values = {'email': sys.argv[2]}
-        data = parse.urlencode(values)
-        data = data.encode('ascii')
-        req = request.Request(url, data)
-        with request.urlopen(req) as response:
-            the_mail = response.read().decode()
-            print(f'{the_mail}')
+if len(sys.argv) > 1:
+    url = sys.argv[1]
+    values = {'email': sys.argv[2]}
+    data = parse.urlencode(values)
+    data = data.encode('ascii')
+    req = request.Request(url, data)
+    with request.urlopen(req) as response:
+        the_mail = response.read().decode()
+        print(f'{the_mail}')
