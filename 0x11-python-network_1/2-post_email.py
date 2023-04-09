4d96e26f4415d0a520562e95d577ea069a6d51b0
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2023-04-09 02:24:38 -0700

    wrote a script that sends a POST request to a url using the mail passed as a parameter

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
-'''Sends a POST request to a url.
+'''Write a Python script that takes in a URL and an email, sends a
+POST request to the passed URL with the email as a parameter, and 
+displays the body of the response (decoded in utf-8)
+The email must be sent in the email variable
+You must use the packages urllib and sys
+You are not allowed to import packages other than urllib and sys
+You don’t need to check arguments passed to the script (number or type)
+You must use the with statement
 Usage:
    ./2-post_email.py <url> <mail>
 Author:
