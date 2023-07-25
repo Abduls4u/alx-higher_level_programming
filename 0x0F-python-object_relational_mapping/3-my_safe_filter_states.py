2bc41ba4c93b68406e68fe609504465b4c7d51f5
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2023-07-25 07:31:51 -0700

    Re-attempt ORM tasks

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 ''' Script that lists all states from the database hbtn_0e_0_usa
-that matches the 4th command line arguments
+that starts with N
 Usage:
     ./3-my_safe_filter_states.py
 Author:
@@ -10,18 +10,25 @@ import MySQLdb
 import sys
 
 
-def list_states_name():
-    '''lists all the states in the db that matches sys.argv[4] '''
+def list_states_N():
+    ''' A function that lists all states that matches sys.argv[4]
+    parameters:
+    ***********
+        size(int): length of the square.
+    Return:
+    *******
+        type == void.
+    '''
 
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     query = "SELECT * FROM states WHERE name LIKE %s"
-    place_holder_value= sys.argv[4]
+    place_holder_value = sys.argv[4]
     cursor.execute(query, (place_holder_value, ))
     states = cursor.fetchall()
     for state in states:
         print(state)
 
 
-list_states_name()
+list_states_N()
